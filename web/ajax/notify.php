cfb94826f52c676a87837f8112e141edd7d174e0
sm00f <borttenburger@sm00fs-Mac.local>
2011-05-09 21:12:21 +0200

    email bugfix

---
+++ web/ajax/notify.php
@@ -0,0 +1,29 @@
+<?php session_start();
+ 
+// ** set configuration
+include('../../config/config.general.php');
+// ** connect to database
+include('../classes/connect.db.php');
+// ** database functions
+include('../classes/database.class.php');
+// ** localization functions
+include('../classes/local.class.php');
+// translate to selected language
+translateSite(substr($_SESSION['language'],0,2),'../');
+// ** all database queries
+include('../classes/db_queries.db.php');
+// ** set configuration
+include('../../config/config.inc.php');
+
+// check for new reservations
+$new = querySQL('notifications');
+
+if($new){
+	$message = _recent_reservations.":<br/>";
+	foreach ($new as $row) {
+		$message .= substr($row->reservation_time,0,5)." ".$row->reservation_guest_name." "._for_." ".$row->outlet_name."<br/>";
+	}
+}
+echo $message;
+
+?>
