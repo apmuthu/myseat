3a5083be20bb1a828374b5d0c70547bf8d6feb08
sm00f <borttenburger@sm00fs-Mac.local>
2011-05-27 06:05:46 +0200

    dansk language added

---
+++ web/lang/dk.php
@@ -0,0 +1,613 @@
+<?
+// This file holds the translation text
+// Text to translate: "define ( '_needle', 'please_translate_me_here' );"
+// Language: da_DK
+// Translator: Mobarak Said <mws@foot-step.dk>
+
+
+//original text: "Open:"
+define ( '_open', 'Åben:' );
+
+//original text: "till"
+define ( '_till', 'til' );
+
+//original text: "o clock"
+define ( '_oclock', 'klokken' );
+
+//original text: "Tautologous with"
+define ( '_tautologous', 'Tautologous with' );
+
+//original text: "Day comment"
+define ( '_day_comment', 'Dagens besked' );
+
+//original text: "Add seats"
+define ( '_add_seats', 'Tilføj plads' );
+
+//original text: "Add tables"
+define ( '_add_tables', 'Tilføj bord' );
+
+//original text: "Capacity"
+define ( '_capacity', 'Kapacitet' );
+
+//original text: "Save"
+define ( '_save', 'Gem' );
+
+//original text: "Delete"
+define ( '_delete', 'Slet' );
+
+//original text: "Reset"
+define ( '_reset', 'Nulstil' );
+
+//original text: "Allow"
+define ( '_allow', 'Tillad' );
+
+//original text: "Edit"
+define ( '_edit', 'Rediger' );
+
+//original text: "Search"
+define ( '_search', 'Søg' );
+
+//original text: "Edit entry"
+define ( '_edit_entry', 'Rediger post' );
+
+//original text: "Edit all entries"
+define ( '_edit_all_entries', 'Rediger alle poster' );
+
+//original text: "OK"
+define ( '_ok_', 'OK' );
+
+//original text: "Cancel"
+define ( '_cancel', 'Annullere' );
+
+//original text: "Overview"
+define ( '_overview', 'Overblik' );
+
+//original text: "Outlet"
+define ( '_outlets', 'Restaurant' );
+
+//original text: "Navigation"
+define ( '_navigation', 'Navigation' );
+
+//original text: "Today"
+define ( '_today', 'I dag' );
+
+//original text: "Print list"
+define ( '_printlist', 'Print liste' );
+
+//original text: "Users"
+define ( '_users', 'Bruger' );
+
+//original text: "Reservations"
+define ( '_reservations', 'Reservationer' );
+
+//original text: "Confirmed reservations"
+define ( '_confirmed_reservations', 'Bekræftet reservationer' );
+
+//original text: "Canceled reservations"
+define ( '_canceled_reservations', 'Annulleret reservationer' );
+
+//original text: "Wait list"
+define ( '_wait_list', 'Vente listet' );
+
+//original text: "Search Guest"
+define ( '_search_guest', 'Søg gæst' );
+
+//original text: "Search results"
+define ( '_search_results', 'Søgeresultater' );
+
+//original text: "Recent reservations"
+define ( '_recent_reservations', 'Seneste reservationer' );
+
+//original text: "Time"
+define ( '_time', 'Tid' );
+
+//original text: "Guest name"
+define ( '_guest_name', 'Gæst navn' );
+
+//original text: "Guests"
+define ( '_pax', 'Gæster' );
+
+//original text: "Phone/Room"
+define ( '_phone_room', 'Tlf/Værelse' );
+
+//original text: "Type"
+define ( '_type', 'Type' );
+
+//original text: "Note"
+define ( '_note', 'Note' );
+
+//original text: "Author"
+define ( '_author', 'Bruger' );
+
+//original text: "Date"
+define ( '_date', 'Dato' );
+
+//original text: "Status"
+define ( '_status', 'Status' );
+
+//original text: "booked "
+define ( '_booked_', 'reserveret ' );
+
+//original text: "people "
+define ( '_people_', 'mennesker ' );
+
+//original text: "for "
+define ( '_for_', 'for ' );
+
+//original text: "or "
+define ( '_or_', 'Eller' );
+
+//original text: "canceled "
+define ( '_canceled_', 'Annulleret ' );
+
+//original text: "Edit reservation"
+define ( '_edit_reservation', 'Rediger reservation' );
+
+//original text: "Add reservation"
+define ( '_add_reservation', 'Tilføj reservation' );
+
+//original text: "Visits"
+define ( '_visits', 'Besøg' );
+
+//original text: "Last visit"
+define ( '_last_visit', 'Sidste besøg' );
+
+//original text: "Do not book any more passants!"
+define ( '_sentence_16', 'Reservere ikke flere gæster!' );
+
+//original text: "Existing outlets"
+define ( '_existing_outlets', 'Eksisterende restauranter' );
+
+//original text: "Add user"
+define ( '_add_user', 'Tilføj bruger' );
+
+//original text: "Add outlet"
+define ( '_add_outlet', 'Tilføj restaurant' );
+
+//original text: "Name"
+define ( '_name', 'Navn' );
+
+//original text: "Description"
+define ( '_description', 'Beskrivelse' );
+
+//original text: "Seats"
+define ( '_seats', 'Pladser' );
+
+//original text: "Tables"
+define ( '_tables', 'Border' );
+
+//original text: "Open time"
+define ( '_open_time', 'Åbningstid' );
+
+//original text: "Close time"
+define ( '_close_time', 'Lukke tid' );
+
+//original text: "Season start"
+define ( '_season_start', 'Sæson start' );
+
+//original text: "Season end"
+define ( '_season_end', 'Sæson slut' );
+
+//original text: "Day off"
+define ( '_day_off', 'Lukke dage' );
+
+//original text: "New entry has been added !"
+define ( '_new_entry', 'Ny reservation er tilføjet !' );
+
+//original text: "Entry has been edited !"
+define ( '_entry_edited', 'Reservation er blevet redigeret !' );
+
+//original text: "Entry deleted!"
+define ( '_entry_deleted', 'Reservation er slettet !' );
+
+//original text: "Nothing deleted!"
+define ( '_nothing_deleted', 'Intet slettet !' );
+
+//original text: "Do you really want to delete?"
+define ( '_sentence_2', 'Ønsker du virkelige at slette?' );
+
+//original text: "Please enter the password"
+define ( '_enter_password', 'Venligst indtast password' );
+
+//original text: "Wrong password !"
+define ( '_wrong_password', 'Forkert password !' );
+
+//original text: "Week"
+define ( '_week', 'Week' );
+
+//original text: "Occupancy per week"
+define ( '_occupancy_per_week', 'Beliggenhed per uge' );
+
+//original text: "Guest type per month"
+define ( '_guest_type_per_month', 'Gæste type per uge' );
+
+//original text: "Occupancy per month"
+define ( '_occupancy_per_month', 'Beliggenhed per måned' );
+
+//original text: "Guest Search"
+define ( '_guest_search', 'Søg gæst' );
+
+//original text: "System"
+define ( '_system', 'System' );
+
+//original text: "Cancelled"
+define ( '_cancelled', 'Annullere' );
+
+//original text: "Comments"
+define ( '_comments', 'Kommentar' );
+
+//original text: "Statistics"
+define ( '_statistics', 'Statistik' );
+
+//original text: "Export"
+define ( '_export', 'Eksporter' );
+
+//original text: "Info"
+define ( '_info', 'Info' );
+
+//original text: "Table"
+define ( '_table', 'Bord' );
+
+//original text: "Changes"
+define ( '_changes', 'Ændre' );
+
+//original text: "Guest summary"
+define ( '_guest_summary', 'Gæst resumé' );
+
+//original text: "Tables summary"
+define ( '_tables_summary', 'Bord resumé' );
+
+//original text: "Parties summary"
+define ( '_parties_summary', 'Parti resumé' );
+
+//original text: "Dollar Revenue summary"
+define ( '_dollar_revenue_summary', 'Indtægter resumé' );
+
+//original text: "Close Window"
+define ( '_close_window', 'Luk Vindue' );
+
+//original text: "Multi Booking<br/>repeat till:"
+define ( '_multi_booking', 'Multi Booking <br/> gentage indtil:' );
+
+//original text: "recurring"
+define ( '_recurring', 'tilbagevendende' );
+
+//original text: "Information !"
+define ( '_information', 'Information !' );
+
+//original text: "Move reservation to"
+define ( '_move_reservation_to', 'Flyt reservation til' );
+
+//original text: "Attention: This table number is already assigned!"
+define ( '_sentence_5', 'OBS: Dette bordnummer er allerede tildelt!' );
+
+//original text: "The restaurant has already opened. Please contact the Maitre!"
+define ( '_sentence_6', 'Restauranten har allerede åbnet. Kontakt venligst Maitre!' );
+
+//original text: "Are you sure to reserve such big table? From "
+define ( '_sentence_8', 'Er du sikker på at reservere så stort et bord ? Fra ' );
+
+//original text: "persons we recommend from a quality reason a menu!"
+define ( '_sentence_9', 'personer anbefaler vi fra en menu!' );
+
+//original text: "Paid"
+define ( '_paid', 'Betalt' );
+
+//original text: "shipped"
+define ( '_shipped', 'Sendt' );
+
+//original text: "Address"
+define ( '_adress', 'Adresse' );
+
+//original text: "ZIP Code-City"
+define ( '_area_code', 'POst nr. - By' );
+
+//original text: "Email"
+define ( '_email', 'Email' );
+
+//original text: "Paid by"
+define ( '_paid_by', 'Betalt af' );
+
+//original text: "Parking"
+define ( '_parking', 'Parkering' );
+
+//original text: "Discount"
+define ( '_discount', 'Rabat' );
+
+//original text: "Password"
+define ( '_password', 'Password' );
+
+//original text: "Ticket system"
+define ( '_ticket_system', 'Billet system' );
+
+//original text: "Price"
+define ( '_ticket_price', 'Pris' );
+
+//original text: "Payment"
+define ( '_payment', 'Betaling' );
+
+//original text: "Shipping"
+define ( '_shipping', 'Forsendelse' );
+
+//original text: "Sent"
+define ( '_sent', 'Sent' );
+
+//original text: "Timecontrol"
+define ( '_timecontrol', 'Tidskontrol' );
+
+//original text: "Online booking"
+define ( '_webform', 'Online booking' );
+
+//original text: "Tautologous booking: This guest has a second booking on this day"
+define ( '_tautologous_booking', 'Booking: Denne gæst har en anden booking på denne dag' );
+
+//original text: "This is an older reservation."
+define ( '_sentence_11', 'Dette er en gammel reservation.' );
+
+//original text: "Reservation was done after maitre message."
+define ( '_sentence_13', 'Reservation var gennemført efter maitre besked.' );
+
+//original text: "Sorry, something went wrong!"
+define ( '_sorry', 'Beklager, noget gik galt!' );
+
+//original text: "General"
+define ( '_general', 'Generalt' );
+
+//original text: "Fill out for sending a confirmation."
+define ( '_fill_out', 'Udfyld for at sende en bekræftelse.' );
+
+//original text: "Confirmation Email"
+define ( '_confirmation_email', 'Beskræftelses Email' );
+
+//original text: "Database"
+define ( '_database', 'Database' );
+
+//original text: "Path"
+define ( '_path', 'Sti' );
+
+//original text: "Language"
+define ( '_language', 'Sprog' );
+
+//original text: "Title"
+define ( '_title', 'Titel' );
+
+//original text: "Booking number"
+define ( '_booknum', 'Booking number' );
+
+//original text: "ZIP"
+define ( '_zip', 'Post nr.' );
+
+//original text: "English"
+define ( '_english', 'Engelsk' );
+
+//original text: "US-English"
+define ( '_international', 'US-Engelsk' );
+
+//original text: "till"
+define ( '_till_', 'til' );
+
+//original text: "at"
+define ( '_at_', 'på' );
+
+//original text: "reserved"
+define ( '_reserved', 'reseveret' );
+
+//original text: "Active"
+define ( '_active', 'Aktiv' );
+
+//original text: "Retype"
+define ( '_retype', 'Skriv' );
+
+//original text: "Cuisine Style"
+define ( '_cuisine_style', 'Køkken stil' );
+
+//original text: "Special Event"
+define ( '_sp_events', 'Special Event' );
+
+//original text: "Subject"
+define ( '_subject', 'Emne' );
+
+//original text: "Open to"
+define ( '_open_to', 'Åben til' );
+
+//original text: "Contact"
+define ( '_contact', 'Kontakt' );
+
+//original text: "Start advertise"
+define ( '_advertise_start', 'Start reklamering' );
+
+//original text: "Days"
+define ( '_days', 'Days' );
+
+//original text: "Reservation confirmation for"
+define ( '_email_subject', 'Reservation bekræftelse for' );
+
+//original text: "Dear Mr."
+define ( '_dear_mr', 'Kære Hr.' );
+
+//original text: "Dear Mrs."
+define ( '_dear_mrs', 'Kære Fru.' );
+
+//original text: "Dear Sirs and Madams"
+define ( '_dear_sirs_and_madams', 'Kære Fru & Hr' );
+
+//original text: "Dear Family"
+define ( '_dear_family', 'Kære Familien' );
+
+//original text: "Property"
+define ( '_property', 'Fysisk Restaurant' );
+
+//original text: "Mr."
+define ( '_M_', 'Hr.' );
+
+//original text: "Mrs."
+define ( '_W_', 'Fru..' );
+
+//original text: "Fam."
+define ( '_F_', 'Fam.' );
+
+//original text: "Company"
+define ( '_C_', 'Firma' );
+
+//original text: "Invoice"
+define ( '_mail', 'Faktura' );
+
+//original text: "Room"
+define ( '_room', 'Værelse' );
+
+//original text: "Inhouse Guest"
+define ( '_HG_', 'Husets gæst' );
+
+//original text: "Passer by"
+define ( '_PASS_', 'Forbi kommende' );
+
+//original text: "Walk in"
+define ( '_WALK_', 'Walk in' );
+
+//original text: "Create"
+define ( '_create', 'Opret' );
+
+//original text: "Year"
+define ( '_year', 'År' );
+
+//original text: "created at"
+define ( '_created', 'Oprette den' );
+
+//original text: "Back"
+define ( '_back', 'Tilbage' );
+
+//original text: "Detail"
+define ( '_detail', 'Detaljer' );
+
+//original text: "Passerby"
+define ( '_passerby', 'Forbi kommende' );
+
+//original text: "Residence time"
+define ( '_duration', 'Opholdstid' );
+
+//original text: "Guest History"
+define ( '_history', 'Gæste historik' );
+
+//original text: "No"
+define ( '_no_', 'Nr' );
+
+//original text: "OK"
+define ( '_yes_', 'OK' );
+
+//original text: "NYA,Arrived,Seated,Parked,Departed,No Show"
+define ( '_statuslist', 'endnu ankommet,Ankommet,Siddende,Parkeret,Gået,Udblev' );
+
+//original text: "delete series"
+define ( '_delete_all_entries', 'Batch slet' );
+
+//original text: "Settings"
+define ( '_settings', 'Indstillinger' );
+
+//original text: "Hello"
+define ( '_hello', 'Hallo' );
+
+//original text: "City"
+define ( '_city', 'By' );
+
+//original text: "Phone"
+define ( '_phone', 'Telefon' );
+
+//original text: "Facsimile"
+define ( '_fax', 'Facsimile' );
+
+//original text: "Country"
+define ( '_country', 'Land' );
+
+//original text: "Image"
+define ( '_img', 'Billed' );
+
+//original text: "free"
+define ( '_free', 'Fri' );
+
+//original text: "Thank you very much for your reservation in our %s. With pleasure we confirm your reservation for %s person(s) on %s at %s. If you want cancel your reservation please use your booking number \'%s\' at the homepage. We wish you an enjoyable stay at our restaurant and are looking forward to your visit! Yours %s"
+// LEAVE THIS TEXT AS IS ! DO NOT TRANSLATE!
+define ( '_email_confirmation_en', 'Thank you very much for your reservation in our %s. With pleasure we confirm your reservation for %s person(s) on %s at %s.<br/>If you want cancel your reservation please use your booking number \'%s\' at the homepage.<br/>We wish you an enjoyable stay at our restaurant and are looking forward to your visit!<br/><br/>Yours<br/>%s' );
+
+//original text: "Reservation confirmation for "
+// LEAVE THIS TEXT AS IS ! DO NOT TRANSLATE!
+define ( '_email_subject_en', 'Reservation confirmation for ' );
+
+//original text: "Thank you very much for your reservation in our %s. With pleasure we confirm your reservation for %s person(s) on %s at %s. If you want cancel your reservation please use your booking number \'%s\' at the homepage. We wish you an enjoyable stay at our restaurant and are looking forward to your visit! Yours %s"
+define ( '_email_confirmation', 'Mange tak for din reservation i vores% s. Det er med fornøjelse at vi bekræfter din reservation for %s person (er) på %s på %s. <br/> Hvis du at ønsker annullere din reservation bedes du bruge dit reservationsnummer \'% s\' på hjemmesiden. <br/> Vi ønsker dig et behageligt ophold på vores restaurant og ser frem til dit besøg! <br/> venlig <br/>%s' );
+
+//original text: "before"
+define ( '_before', 'før' );
+
+//original text: "Dear Mr."
+// LEAVE THIS TEXT AS IS ! DO NOT TRANSLATE!
+define ( '_dear_mr_en', 'Dear Mr.' );
+
+//original text: "Dear Mrs."
+// LEAVE THIS TEXT AS IS ! DO NOT TRANSLATE!
+define ( '_dear_mrs_en', 'Dear Mrs.' );
+
+//original text: "Dear Sirs and Madams"
+// LEAVE THIS TEXT AS IS ! DO NOT TRANSLATE!
+define ( '_dear_sirs_and_madams_en', 'Dear Sirs and Madams' );
+
+//original text: "Dear family"
+// LEAVE THIS TEXT AS IS ! DO NOT TRANSLATE!
+define ( '_dear_family_en', 'Dear Family' );
+
+//original text: "The username"
+define ( '_already_user_1', 'Brugernavnet' );
+
+//original text: "does already exist."
+define ( '_already_user_2', 'eksisterer allerede.' );
+
+//original text: "Dear %s, Thank you for registering at mySeat! Your username is %s . Your password is %s . Your email address is set to %s . Follow us on Twitter, Facebook, and more here: http://www.openmyseat.com All the best, The mySeat Team "
+define ( '_user_email_confirmation', 'Kære% s, Tak for din registrering hos Aristo booking! Dit brugernavn er% s. Din adgangskode er% s. Din e-mail-adresse er sat til% s. Følg os på Twitter, Facebook og mere her: http://www.restaurantaristo.dk. Bedste hilsner, Aristo ' );
+
+//original text: "Hello %s,<br/><br/>You just signed up for mySeat. Please follow this link to confirm that this is your e-mail address:<br/><br/><a title="Confirm Account" href="%sweb/confirm.php?c=%s">%sweb/confirm.php?c=%s</a><br/><br/>Thank you,<br/><br/>The mySeat Team"
+define ( '_user_activation_email', 'Hej% s, <br/> Du har lige tilmeldt dig Aristo booking. Følg venligst dette link for at bekræfte, at dette er din e-mail-adresse: <br/> <a title="Confirm Account" href="%sweb/confirm.php?c=%s">% sweb / confirm.php? c =% ??s </ a> <br/> Tak, <br/> Aristo' );
+
+//original text: "You have already confirmed this email address."
+define ( '_errorMessage_all', 'Du har allerede bekræftet denne e-mail-adresse.' );
+
+//original text: "You are trying to confirm an invalid email address."
+define ( '_errorMessage_no', 'Du forsøger at bekræfte en ugyldig e-mail-adresse.' );
+
+//original text: "Your user account has been confirmed!"
+define ( '_errorMessage_yes', 'Din brugerkonto er blevet bekræftet!' );
+
+//original text: "Break"
+define ( '_break', 'Pause' );
+
+//original text: "Specific"
+define ( '_specific', 'Specifikke' );
+
+//original text: "Dashboard"
+define ( '_dashboard', 'Dashboard' );
+
+//original text: "I would like to receive informations by email."
+define ( '_reservation_advertise', 'Jeg vil gerne modtage information via e-mail.' );
+
+//original text: "Dr."
+define ( '_DR_', 'Dr.' );
+
+//original text: "Prof."
+define ( '_PROF_', 'Prof.' );
+
+//original text: "Online"
+define ( '_online', 'Online' );
+
+//original text: "Ad"
+define ( '_ads', 'Tilføj' );
+
+//original text: "Print"
+define ( '_print', 'Print' );
+
+//original text: "Top"
+define ( '_top', 'Top' );
+
+//original text: "Traffic sources"
+define ( '_trsources', 'Trafik kilde' );
+
+//original text: "Website"
+define ( '_website', 'Hjemmeside' );
+?>
