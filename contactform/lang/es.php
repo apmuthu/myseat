a72c44c86bcc97d6e92ce4754a027463bd05f625
Bernd Orttenburger <beo@myseat.us>
2011-09-08 20:18:41 +0200

    contactform

---
+++ contactform/lang/es.php
@@ -0,0 +1,150 @@
+<?php
+	//LANGUAGE: ESPA—OL
+
+	// PLEASE USE HTML ENTITIES HERE !
+	// e.g.: ö = &ouml;
+
+	//----------------------------
+	// HEADER
+	//----------------------------
+
+	// The text before the list of the available language -- "View this page in"
+	$lang["change_language"] = 'Ver esta pagina en';
+
+	// List the available languages sorted in an array, the keys are the slug names that correspond to each language file
+	$lang["available_language"] = array(
+		"en" => "English",
+		"es" => "Espa&ntilde;ol",
+		"de" => "Deutsch",
+		"fr" => "Fran&ccedil;aise",
+		"nl" => "Nederlandse",
+		"cn" => "Chinese",
+		"dk" => "Dansk",
+	);
+
+	// The contact information in the top-right of the page -- "www.openmyseat.com <br /> www.myseat.us"
+	$lang["contact_info"] = 'www.openmyseat.com <br /> www.myseat.us';
+
+
+
+	//----------------------------
+	// CONTENT
+	//----------------------------
+
+	// The page title -- "Select your prefered restaurant"
+	$lang["title"] = 'Reserva por Internet';
+
+	// A line of text before the contact form -- "Make an instant reservation now! <br/>For private parties, large bookings or all other queries please call us."
+	$lang["contact_form_intro"] = '&iexcl;Reserva Ahora!
+	<br/>Para fiestas privados, revservas para grupos y cualquier otra petici&oacute;n llamarnos.';
+
+	// Default text of the contact form -- "Name or Group Name"
+	$lang["contact_form_name"] = 'Nombre de la reserva';
+
+	// Default text of the contact form -- "E-mail address"
+	$lang["contact_form_email"] = 'Correo electronico';
+	
+	// Default text of the contact form -- "Security question"
+	$lang["security_question"] = 'Pregunta de seguridad';
+	
+	// Default text of the contact form -- "I would like to receive informations by email."
+	$lang["contact_form_advertise"] = 'S&iacute;, deseo recibir noticias del restaurante por correo.';
+
+	// Default text of the contact form -- "Reservation notes"
+	$lang["contact_form_notes"] = 'Notas para la reserva';
+
+	// Default text of the contact form -- "Restaurant"
+	$lang["contact_form_restaurant"] = 'Restaurante';
+
+	// Default text of the contact form -- "Time"
+	$lang["contact_form_time"] = 'Hora';
+
+	// Default text of the contact form -- "Phone"
+	$lang["contact_form_phone"] = 'Tel&eacute;fono*';
+
+	// Default text of the contact form -- "Number of persons"
+	$lang["contact_form_pax"] = 'Numero de comensales';
+
+	// Default text of the contact form -- "Title"
+	$lang["contact_form_title"] = 'Titulo';
+
+	// Default text for the 'captcha' field of the contact form -- "Security question"
+	$lang["contact_form_captcha"] = 'Pregunta de seguridad';
+
+	// The 'Send' button -- "Book"
+	$lang["contact_form_send"] = 'Reservar';
+
+	// The 'Back' button -- "Back"
+	$lang["contact_form_back"] = 'Atras';
+
+	// The 'Cancel' button -- "Cancel"
+	$lang["contact_form_cxl"] = 'Cancelar Reserva';
+
+	// Message text -- "Booking number"
+	$lang["book_num"] = 'Numero de reserva';
+
+	// The success message -- "Thank you for your reservation. An email confirmation was sent. Your Booking number is:"
+	$lang["contact_form_success"] = 'Gracias por la reserva. Hemos enviado un correo eletronico de confirmaci&oacute;n. <br/> Tu numero de reserva es: ';
+
+	// The full outlet message -- "The restaurant can\'t be booked for this date, please try another date."
+	$lang["contact_form_full"] = 'No se puede reservar para esta fecha, por favor probar con otra fecha.';
+
+	// The failure message -- "An error occurred while booking, please try again."
+	$lang["contact_form_fail"] = 'Hab&iacute;a un fallo durante el process de reserva, por favor intentarlo de nueva.';
+
+	// The success message -- "Your reservation has been canceled! Please visit us again!<br/>Feel free to contact us if you have any further questions or concerns."
+	$lang["cxl_form_success"] = '°Tu reserva ha sido cancelado! Please visit us again!<br/> Por favor contactarnos si para cualquier otra pregunta o asunto.';
+
+	//----------------------------
+	// CONTACT FORM
+	//----------------------------
+
+	// The titles
+	$lang["_M_"] = 'Sr.'; // Mr.
+	$lang["_DR_"] = 'Dr.'; // Dr.
+	$lang["_PROF_"] = 'Prof.'; // Prof.
+	$lang["_W_"] = 'Sra.'; // Mrs.
+	$lang["_F_"] = 'Familia'; // Family
+	$lang["_C_"] = 'Grupo'; // Group
+
+	//----------------------------
+	// BOOKING
+	//----------------------------
+
+	// The emails subject -- "Reservation confirmation for"
+	$lang["email_subject"] = 'Confirmaci&oactue;n de reserva para';
+
+	// The page title -- "<strong>Reservation Process</strong>"
+	$lang["conf_title"] = 'Processo de Reserva';
+
+	// The page title -- "<strong>Cancel Your Reservation</strong>"
+	$lang["cxl_title"] = 'Cancelar Tu Reserva';
+
+	// A subline of text I -- "Reservation progress for"
+	$lang["conf_intro"] = 'Progresso de reserva para';
+
+	// A subline of text -- "Directly clear your reservation."
+	$lang["cxl_intro"] = 'Para cancelar tu reserva, por favor insertar tu numero de reserva y el correo hacia donde estaba enviada.';
+
+	// A subline of text II -- "at"
+	$lang["_at_"] = 'a';
+
+	// Day off error message
+	$lang["error_dayoff"] = 'No podemos tomar reservas para esta d&iacute;a.';
+
+	//----------------------------
+	// FOOTER
+	//----------------------------
+
+	// The content on the first footer 
+	$lang["footer_one"] =  '<p><small>Si hay alguna duda o peticiones especiales, por favor no dude en contactar<br/>con nosotros con el número de teléfono %s o mediante correo electrónico a <a href="mailto:%s">%s</a>.</small></p>';
+
+	//----------------------------
+	// MINI-FOOTER
+	//----------------------------
+
+	// The copyright text, do not change! -- "<a href="#">Copyright &copy; 2010 mySeat</a>"
+	$lang["minifooter_copyright"] = '&copy; 2010 by <a href="http://www.openmyseat.com">MYSEAT</a> under the GPL license';
+
+
+?>
