68c6942a6ad179162cd2159e1c24f0810b827547
Bernd Orttenburger <beo@myseat.us>
2011-09-19 07:31:24 +0200

    v0.190

--- web/main_page.php
+++ web/main_page.php
@@ -22,7 +22,7 @@ This file is part of mySeat.
 /** Login **/
 // ** set configuration
 	//Software version
-	$sw_version = 'v0.188';
+	$sw_version = 'v0.190';
 	
 	include('../config/config.general.php');
 	
